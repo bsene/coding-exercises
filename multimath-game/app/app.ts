2e49ca7f5964a32279ade110b77c876c4a8a2ef6
Birame SENE <bsene@pm.me>
2021-03-10 01:25:19 +0100

    Typescript, Getting started course: Writing Better Functions with TypeScript

--- multimath-game/app/app.ts
+++ multimath-game/app/app.ts
@@ -1,16 +1,50 @@
+const logMessage = (message: string) => console.log(message);
+const logError = (err: string) => console.error(err);
 function startGame(): void {
+  const playerName: string = getInputValue("playerName") as string;
 
-    const playerName = 'Andy';
-    logPlayer(playerName);
-
-    const messageElement: HTMLElement | null = document.getElementById('messages')
-    if(typeof messageElement !== null) {
-        messageElement.innerText = 'Welcome to MultiMath ! Starting a new game...'
-    }
+  logPlayer(playerName);
+  postScore(1000, playerName);
+  postScore(-3, playerName);
 }
 
-function logPlayer(name) {
-    console.log(`New game starting for player: ${name}`)
+function logPlayer(name = "anonymous"): void {
+  console.log(`New game starting for player: ${name}`);
 }
 
-document.getElementById('startGame')!.addEventListener('click', startGame)
+function getInputValue(elementID: string): string {
+  const element: HTMLInputElement =
+    (document.getElementById(elementID) as HTMLInputElement);
+
+  if (element.value === "string") return "";
+
+  return element.value;
+}
+
+function postScore(score: number, playerName: string): void {
+  if (playerName.length === 0) return;
+
+  let logger: (value: string) => void;
+
+  logger = logMessage;
+  if (score < 0) logger = logError;
+
+  const scoreElement: HTMLElement =
+    (document.getElementById("postedScores") as HTMLElement);
+  scoreElement.innerText = `${score} - ${playerName}`;
+
+  logger(`Score: ${score}`);
+}
+
+const startGameElement: HTMLElement | null = document.getElementById(
+  "startGame",
+);
+if (startGameElement instanceof HTMLElement) {
+  startGameElement.addEventListener("click", startGame);
+}
+const formGameElement: HTMLElement | null = document.getElementById("formGame");
+if (formGameElement instanceof HTMLElement) {
+  formGameElement.addEventListener("submit", function (e) {
+    e.preventDefault();
+  });
+}
